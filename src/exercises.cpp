1fba26f4d8f17aa352fbfe62c2beb54faf1d499d
JorgeMartinezSanchez <159574519+JorgeMartinezSanchez@users.noreply.github.com>
2024-04-01 09:50:30 -0400

    ex1.0

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -4,16 +4,13 @@
 
 using namespace std;
 
-void exercise_1(string s1) {
-  // TODO: YOUR CODE HERE
+void exercise_1(string text) {
     int indice = 0;
 	
 	for (int i = 0; i < text.length(); i++){
 		if (text[i] == ' '){
 			cout<<i;
 		}
-
-
 	}
 	cout<<" "<<text.length()<<endl;
 }
