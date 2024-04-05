e65e5d97e8494385318c97e8650d8e1f51f5aaac
JorgeMartinezSanchez <159574519+JorgeMartinezSanchez@users.noreply.github.com>
2024-04-05 12:24:23 -0400

    x1.5

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -7,10 +7,10 @@ using namespace std;
 void exercise_1(string s1) {
 	for (int i = 0; i < s1.length(); i++){
 		if (s1[i] == ' '){
-			cout<<i;
+			cout<< i<<endl ;
 		}
 	}
-  	cout<<" "<<s1.length()<<endl;
+	cout << s1.length() << endl;
 }
 
 void exercise_2(string s1) {
